80797035b390a07f77c5f9413a85d7677ef2f4b1
Kimbeomju <kbj52264291@gmail.com>
2022-03-19 23:08:27 +0900

    폴더 세부화 작업

--- Side_Project/Assets/01.Scripts/ColorInHierarchy.cs
+++ Side_Project/Assets/01.Scripts/ColorInHierarchy.cs
@@ -2,12 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class ColorInHierarchy : MonoBehaviour
 {
-    public GameObject a;
-    //
-    // S
-    // tart is called before the first frame update
+    // Start is called before the first frame update
     void Start()
     {
         

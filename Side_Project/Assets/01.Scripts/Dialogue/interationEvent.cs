89455fceb701495651f60aa5fc76a5acea6374d2
Kimbeomju <kbj52264291@gmail.com>
2022-03-20 08:49:13 +0900

    다이얼로그 스크립트, 인터렉션 이벤트 개발, 데이터베이스 매니저 스크립트 추가

---
+++ Side_Project/Assets/01.Scripts/Dialogue/interationEvent.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class interationEvent : MonoBehaviour
+{
+    public int lineY;
+    public int s_lineY;
+    [SerializeField] private DialogueEvent dialogue;
+   // [SerializeField] private Select
+
+       
+    // public Dialgoue[] GetDialgoues()
+    // {
+    //     dialgoue.dialgoues = DatabaseManager.instance.GetDialgoues(1, lineY);
+    //     return dialgoue.dialgoues;
+    // }
+
+    //     public SelectDialogue[] GetSelectes()
+    // {
+    //     select.Selecter = DatabaseManager.instance.GetSelects(1, s_lineY);
+    //     return select.Selecter;
+    // }
+
+}

03a50cdfec883eafcc4ac5e6cb3fa6891754694d
Kimbeomju <kbj52264291@gmail.com>
2022-03-30 11:34:35 +0900

    CameraResolution, ShakeCamera 스크립트 추가

---
+++ Side_Project/Assets/1.Scripts/Camera/ShakeCamera.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class ShakeCamera : MonoBehaviour
+{
+    private CinemachineVirtualCamera cmVcam;
+    private CinemachineBasicMultiChannelPerlin noise;
+
+    private Coroutine co = null;
+
+    private void Awake()
+    {
+        cmVcam = GetComponent<CinemachineVirtualCamera>();
+        noise = cmVcam.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+    }
+
+    public void SetShake(float intense, float during)
+    {
+        noise.m_AmplitudeGain = intense;
+        if (co != null) StopCoroutine(co);
+        co = StartCoroutine(ReduceShake(during));
+    }
+
+    IEnumerator ReduceShake(float during)
+    {
+
+        while (noise.m_AmplitudeGain >= 0.2f)
+        {
+            noise.m_AmplitudeGain = Mathf.Lerp(noise.m_AmplitudeGain, 0, Time.deltaTime / during);
+            yield return null;
+        }
+        noise.m_AmplitudeGain = 0;
+    }
+}

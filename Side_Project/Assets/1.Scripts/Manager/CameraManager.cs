bafba3d7eb6c4831681e3067c743e88936cf9bdb
Kimbeomju <kbj52264291@gmail.com>
2022-03-30 12:35:27 +0900

    제네릭 싱글톤 추가, 컬러 하이라키, 폴더 구조 변환

--- Side_Project/Assets/1.Scripts/Manager/CameraManager.cs
+++ Side_Project/Assets/1.Scripts/Manager/CameraManager.cs
@@ -5,13 +5,21 @@ using UnityEngine;
 public class CameraManager : Singleton<CameraManager>
 {
     private ShakeCamera shakeCam;
+    private Camera mainCam;
 
     protected override void Awake()
     {
         // base.Awake();
         if (shakeCam != null)
             shakeCam = GetComponent<ShakeCamera>();
+
+        if (mainCam != null)
+            mainCam = Camera.main;
     }
+
+
+
+
 
     public static void ShakeCam(float intense, float during)
     {
